0ddae2b834d39fbd743b05d43546355bfa4cb968
plinioaltoe <plinioaltoe@yahoo.com.br>
2019-04-11 22:45:18 -0300

    redirect

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import { Redirect } from 'react-router-dom'
 import Header from '../../components/Header'
 import UserInputs from '../../components/UserInputs'
 import PreferencesList from '../../components/PreferencesList'
@@ -6,18 +7,25 @@ import PreferencesList from '../../components/PreferencesList'
 import { Container, Button, Text } from './styles'
 
 export default class Profile extends Component {
-  state = {}
+  state = {
+    redirect: false,
+    route: '',
+  }
+
+  goTo = route => this.setState({ redirect: true, route })
 
   render() {
+    const { redirect, route } = this.state
     return (
       <Fragment>
+        {redirect && <Redirect push to={route} />}
         <Header />
         <Container>
           <form>
             <UserInputs display="profile" />
             <Text>Preferências</Text>
             <PreferencesList />
-            <Button onClick={() => {}}>Salvar</Button>
+            <Button onClick={() => this.goTo('/dashboard')}>Salvar</Button>
           </form>
         </Container>
       </Fragment>
