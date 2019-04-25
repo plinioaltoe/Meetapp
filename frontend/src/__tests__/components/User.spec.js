fc631fa7a43a26847f606e00b82195779400cdbd
plinioaltoe <plinioaltoe@yahoo.com.br>
2019-04-25 07:39:38 -0300

    testes

---
+++ frontend/src/__tests__/components/User.spec.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { Provider } from 'react-redux'
+import createStore from 'redux-mock-store'
+
+const mockStore = createStore()
+
+// import User
